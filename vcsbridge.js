783db91601765306e9a90e31fc8c9cd3279de2fa
bobdidley <james.austin@student.csulb.edu>
2021-02-14 19:36:19 -0800

    Artifact ID draft

--- vcsbridge.js
+++ vcsbridge.js
@@ -34,3 +34,49 @@ var app = express();
             console.log("vcsbridge.js listening on port 3000!");
         }
     );
+
+/**
+ * Artifact ID Generator
+ * Generates a filename according to file data passed in through form
+ */
+function artID() {
+    var rootName = __dirname; // A - root name // fs.dir.path? // need to change path name to respective file
+    var fileData = fs.readFileSync(rootName + '/repo.txt', 'utf8'); // repo.txt needs to change
+    var fileLength = fileData.length();
+
+    // needs to be changed to respective file
+    var D = 'repo';
+    var E = 'txt';
+
+    var iter = 0;
+    var hexTotal = 0;
+    var iterTracker = 4;
+    var r = 0;
+    while (!fileData) { // idk how to check for EOF
+        // iter++;
+        r = ++iter % iterTracker;
+        if (r == 0) {
+            hexTotal += fileData.charAt(iter) * 3;
+        }
+        else if (r == 1 || r == 3) {
+            hexTotal += fileData.charAt(iter) * 7;
+        }
+        else if (r == 2) {
+            hexTotal += fileData.charAt(iter) * 11;
+        }
+    }
+    var C = hexTotal.toString(16); // C - hex output, needs to be truncated for overflow
+
+    function hexConvert (text) {
+        var hex = '';
+        for (var i = 0; i < text.length; ++i) {
+            hex += text.charAt(i).toString(16);
+        }
+        return hex;
+    }
+
+    var A = hexConvert(rootName); // A - hex output
+    var B = hexConvert(fileLength); // B - hex output
+
+    console.log('' + A + '/' + B + '/' + C + '/' + D + '.' + E);
+}
