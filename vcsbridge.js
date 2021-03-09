d5c78c027956d2c5d486a050d17889dde392decf
bobdidley <james.austin@student.csulb.edu>
2021-03-09 13:10:13 -0800

    RepoName edit

--- vcsbridge.js
+++ vcsbridge.js
@@ -26,7 +26,8 @@ app.get(
     function(req, res) {
         var repoName = req.query.repo_name; // ! something
         var sourcePath = req.query.source_path; // ! C:/Users/rifat/Desktop/source
-        var targetPath = req.query.target_path + '/' + repoName; // ! C:/Users/rifat/Desktop/CSULB-2021-Spring/target
+        // var targetPath = req.query.target_path + '/' + repoName; // ! C:/Users/rifat/Desktop/CSULB-2021-Spring/target
+        var targetPath = req.query.target_path;
         var base = path.basename(sourcePath); // ? something bc we add folder called 'something' to the end
 
         console.log('Repo Name recieved: ' + repoName);
