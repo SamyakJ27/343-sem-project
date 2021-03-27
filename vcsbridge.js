a9b0a60ffc764e859864ed43b016e5522a4a090f
bobdidley <james.austin@student.csulb.edu>
2021-03-26 19:30:14 -0700

    Listing app.get and Check out button

--- vcsbridge.js
+++ vcsbridge.js
@@ -106,23 +106,19 @@ app.get( // may use different names
 app.get(
     '/get_listing_form',
     function(req, res) {
+        console.log("RUNNING listing_form");
         //const data = fs.readFileSync('inputSourcePathHere', {encoding: 'utf-8', flag:'r'}); 
         // ! put source folder in string so that we can print out all files in that folder
         // ! have user input the folder path
-        res.send("List of Labels");
-        var arrayOfLabels = [];
-        if (arrayOfLabels.length === 0) {
-            res.send("There are no labels to print out!")
-        } else {
-            fs.readdir(data, (err, files) => {
-                files.forEach(file => {
-                    res.send(file);
-                });
-            });
-        }
-        fs.readdirSync(testFolder).forEach(file => {
-            console.log(file);
-        });
+
+        let repoPath = req.query.repo_path;
+        console.log(repoPath);
+        var labeltxtPath = path.join(repoPath, '/.labels.txt');
+        console.log(labeltxtPath)
+
+        var arrayOfLabels = fs.readFileSync(labeltxtPath, 'utf-8');
+        console.log(arrayOfLabels);
+        res.send("List of Labels" + arrayOfLabels);
     }
 );
 
