79604fd16b7139d95682e3137b06919001c78710
bobdidley <james.austin@student.csulb.edu>
2021-05-14 17:41:12 -0700

    let

--- vcsbridge.js
+++ vcsbridge.js
@@ -205,7 +205,7 @@ function merge_out(sourcePath, maniPath, sourceBranch) {
                         gID = fileLine.substring(0, fileLine.indexOf("@") - 1);;
                     }
                 });
-                oldgID = gID;
+                let oldgID = gID;
                 gID = gID.substr(0, gID.lastIndexOf(".")) + "_GM" + gID.substr(gID.lastIndexOf("."));
                 console.log("gID:", gID);
                 fs.copyFile(path.join(repoPath, oldgID), path.join(changes_direc, gID), fs.constants.COPYFILE_FICLONE, function (err) {
